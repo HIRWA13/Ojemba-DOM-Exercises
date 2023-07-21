fc4bc5bb4fbf34b9eb6ea24785066dadb94a53b6
HIRWA13 <iamhirwejr@gmail.com>
2023-07-21 17:00:04 +0200

    modified the hide message directory

--- Hide the message/app.js
+++ Hide the message/app.js
@@ -5,12 +5,4 @@ messages.addEventListener('click', (e) => {
         const message = e.target.parentElement;
         message.style.display = 'none';  
      }
-    // display "no messages" if all messages are hidden
-    const allMessages = document.querySelectorAll('.message');
-    const hiddenMessages = document.querySelectorAll('.message[style="display: none;"]');
-    if (allMessages.length === hiddenMessages.length) {
-        const noMessages = document.createElement('div');
-        noMessages.innerText = 'No messages';
-        messages.appendChild(noMessages);
-    }
 });
